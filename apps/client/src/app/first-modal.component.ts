e24f6352e684250cf3f06ce1416cb5626c6c28f2
daBishMan <jaybishtawi@outlook.com>
2024-07-05 14:28:05 -0600

    chore: Remove unused code and simplify modal functionality

--- apps/client/src/app/first-modal.component.ts
+++ apps/client/src/app/first-modal.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { InstanceOptions, Modal, ModalInterface, ModalOptions } from 'flowbite';
 
 @Component({
   selector: 'app-first-modal',
@@ -8,4 +9,34 @@ import { CommonModule } from '@angular/common';
   templateUrl: './first-modal.component.html',
   styleUrl: './first-modal.component.scss',
 })
-export class FirstModalComponent {}
+export class FirstModalComponent implements OnInit {
+  $modalElement: HTMLElement | null = null;
+
+  modal: ModalInterface | null = null;
+
+  ngOnInit(): void {
+    this.$modalElement = document.querySelector('#modalEl');
+    this.modal = new Modal(
+      this.$modalElement,
+      {
+        placement: 'top-center',
+        backdrop: 'static',
+        closable: false,
+      } as ModalOptions,
+      {
+        id: 'modalEl',
+        override: true,
+      } as InstanceOptions
+    );
+  }
+
+  showModal(): void {
+    console.log('toggle modal method');
+    this.modal?.show();
+  }
+
+  hideModal(): void {
+    console.log('hide modal method');
+    this.modal?.hide();
+  }
+}
--- apps/client/src/app/landing-page.component.ts
+++ apps/client/src/app/landing-page.component.ts
@@ -1,7 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import type { InstanceOptions, ModalInterface, ModalOptions } from 'flowbite';
-import { Modal } from 'flowbite';
 
 @Component({
   selector: 'app-landing-page',
@@ -12,29 +10,7 @@ import { Modal } from 'flowbite';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LandingPageComponent implements OnInit {
-  $modalElement: HTMLElement | null = null;
-
-  modal: ModalInterface | null = null;
-
   ngOnInit(): void {
-    this.$modalElement = document.querySelector('#modalEl');
-    this.modal = new Modal(
-      this.$modalElement,
-      { placement: 'top-center' } as ModalOptions,
-      {
-        id: 'modalEl',
-        override: true,
-      } as InstanceOptions
-    );
-  }
-
-  showModal(): void {
-    console.log('toggle modal method');
-    this.modal?.show();
-  }
-
-  hideModal(): void {
-    console.log('hide modal method');
-    this.modal?.hide();
+    console.log('Landing page component initialized');
   }
 }
